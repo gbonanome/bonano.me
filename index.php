73b5bfbc0aea8b0b8bcce8847897fdf265f363b4
Giulio Bonanome <gbonanome@gmail.com>
2014-02-18 11:09:26 +0100

    Moved GA out of the repo

--- index.php
+++ index.php
@@ -84,11 +84,8 @@
 
         <script src="js/main.js"></script>
 
-        <script>
-            var _gaq=[['_setAccount','UA-44878416-1'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
+        <?php
+            include_once 'ga.php';
+        ?>
     </body>
 </html>
